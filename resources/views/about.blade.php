d5de7c432088953a850b082812785c0d0370e6f3
salwa putri <salwaputri998@gmail.com>
2026-04-14 23:41:05 +0700

    salwa update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,10 @@
-<h1>Tentang Kami</h1>
-<p>Klinik Digital adalah platform layanan kesehatan modern.</p>
-<a href="/home">Kembali ke Home</a>
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+@include('navbar')
+
+<div class="p-6 text-center">
+    <h1 class="text-2xl font-bold">Tentang Kami</h1>
+
+    <img src="{{ asset('images/hospital.jpg') }}" class="mx-auto mt-4 rounded-lg w-60">
+
+    <p class="mt-3 text-gray-600">Platform kesehatan berbasis teknologi.</p>
+</div>
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,5 +1,3 @@
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-
 <nav class="bg-blue-600 p-4 text-white">
     <div class="flex justify-between">
         <h1 class="font-bold">Klinik Digital</h1>
@@ -14,11 +12,3 @@
         </div>
     </div>
 </nav>
-
-<div class="p-6 text-center">
-    <h1 class="text-3xl font-bold text-blue-600">Selamat Datang di Klinik Digital</h1>
-
-    <img src="{{ asset('images/dokter.png') }}" class="mx-auto mt-4 rounded-lg shadow-lg w-60">
-
-    <p class="mt-4 text-gray-600">Layanan kesehatan online terpercaya.</p>
-</div>
