8b3210925c4daf5bd6b92c9e0fbb862a2244af88
arizzrizqi <arizztobio@gmail.com>
2026-04-14 23:47:23 +0700

    annisa update

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,14 @@
+<nav class="bg-blue-600 p-4 text-white">
+    <div class="flex justify-between">
+        <h1 class="font-bold">Klinik Digital</h1>
+        <div class="space-x-4">
+            <a href="/home">Home</a>
+            <a href="/about">About</a>
+            <a href="/service">Service</a>
+            <a href="/contact">Contact</a>
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+            <a href="/dashboard">Dashboard</a>
+        </div>
+    </div>
+</nav>
